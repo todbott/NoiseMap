3d501720a299df769e01b8d42f63da26deca115d
todbott <todd.g@listenfield.com>
2022-06-25 12:44:52 +0900

    First commit of real code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,91 @@
 import React, { Component } from 'react';
+import AudioAnalyzer from './audioAnalyzer';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      audio: null,
+      seconds: 0,
+      samplingInProgress: false,
+      audioArray: Array(1024).fill(0)
+    };
+    this.toggleMicrophone = this.toggleMicrophone.bind(this);
+  }
+
+  async getMicrophone() {
+    const audio = await navigator.mediaDevices.getUserMedia({
+      audio: true,
+      video: false
+    });
+    this.setState({ audio });
+  }
+
+  stopMicrophone() {
+    this.state.audio.getTracks().forEach(track => track.stop());
+    this.setState({ audio: null });
+  }
+
+  toggleMicrophone() {
+
+    this.getMicrophone();
+    this.setState({samplingInProgress: true})
+    const interval = setInterval(() => {
+        this.setState({seconds: this.state.seconds + 1})
+        console.log(this.state.seconds)
+    }, 1000);
+    setTimeout(() => {
+      this.stopMicrophone()
+      clearInterval(interval)
+      this.setState({samplingInProgress: false})
+    }, 11000);
+  }
+
+  atEverySecond = (val) => {
+    
+    let values = this.state.audioArray
+
+    // What are the values this time around?
+    let  newValues = val
+
+    console.log(values[0])
+    console.log(newValues[0])
+
+    for (let v = 0; v < values.length; v++) {
+      if (newValues[v] > values[v]) {
+        values[v] = newValues[v]
+      }
+    }
+
+    this.setState({audioArray: values})
+  }
+
+
+
   render() {
+
+    const frequencyArray = this.state.audioArray.toString().split(",")
+  
     return (
       <div className="App">
         <div className="controls">
-          <button>Get microphone input</button>
+        { this.state.samplingInProgress == false ? 
+          (
+            <button onClick={this.toggleMicrophone}>
+              Start sampling
+            </button>
+          ) : 
+          (
+            <div>Sampling in progress</div>
+          )
+        }       
         </div>
+        {this.state.audio ? <AudioAnalyzer audio={this.state.audio} atEverySecond={this.atEverySecond}/> : ''}
+        {frequencyArray.map((value) => (
+          <div>
+            {value}
+          </div>
+        ))}
       </div>
     );
   }
---
+++ src/audioAnalyzer.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import AudioVisualizer from './audioVisualizer';
+
+class AudioAnalyzer extends Component {
+      constructor(props) {
+        super(props);
+        this.state = { audioData: new Uint8Array(0) };
+        this.tick = this.tick.bind(this);
+        
+    }
+
+    componentWillUnmount() {
+        cancelAnimationFrame(this.rafId);
+        this.analyser.disconnect();
+        this.source.disconnect();
+      }
+
+
+    componentDidMount() {
+        this.audioContext = new (window.AudioContext ||
+          window.webkitAudioContext)();
+        this.analyser = this.audioContext.createAnalyser();
+        this.dataArray = new Uint8Array(this.analyser.frequencyBinCount);
+        this.source = this.audioContext.createMediaStreamSource(this.props.audio);
+
+        this.source.connect(this.analyser);
+        this.rafId = requestAnimationFrame(this.tick);
+
+        
+
+
+      }
+
+     
+      tick() {
+     
+        this.analyser.getByteTimeDomainData(this.dataArray);
+        this.setState({ audioData: this.dataArray });
+        
+        this.props.atEverySecond(this.state.audioData)
+    
+        this.rafId = requestAnimationFrame(this.tick);
+      }
+
+      render() {
+        
+        return <AudioVisualizer audioData={this.state.audioData} />;
+      }
+ 
+}
+
+export default AudioAnalyzer;
